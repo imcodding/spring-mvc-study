ab1e450a52eaa036fa0e991c8f8dbafbbe2149d1
mia <55366664+imcodding@users.noreply.github.com>
2022-04-13 11:07:03 +0900

    [part4] 스프링 MVC 시작

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,17 @@
+package hello.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller // 1. 스프링 빈 등록 2. 스프링 MVC 에서 컨트롤러(핸들러)로 인식
+// = @Component @RequestMapping
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+
+}
